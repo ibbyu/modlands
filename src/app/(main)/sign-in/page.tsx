2893391ac591562b9610b30d79718c53ec07d972
Ibby <ibby364@outlook.com>
2024-03-05 18:13:03 +0000

    feat: discord sign in

---
+++ src/app/(main)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignInCard from './_components/sign-in-card';
+
+const SignInPage = () => {
+  return (
+    <div className='flex justify-center items-center pt-32'>
+      <SignInCard />
+    </div>
+  );
+}
+
+export default SignInPage;
