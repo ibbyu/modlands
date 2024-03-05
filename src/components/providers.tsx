0e4152ca75e2f3326ef7666855bef4b14ccbd762
Ibby <ibby364@outlook.com>
2024-03-05 19:41:40 +0000

    feat: nav loader

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Next13ProgressBar } from 'next13-progressbar';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <SessionProvider>
+        <Next13ProgressBar height="2px" color="#488cfa" options={{ showSpinner: true }} showOnShallow />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
