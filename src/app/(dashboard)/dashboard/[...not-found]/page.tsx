2d56ae149e63a64dcd9ef279814925da846c3859
Ibby <ibby364@outlook.com>
2024-03-05 23:42:31 +0000

    feat: not found pages

---
+++ src/app/(dashboard)/dashboard/[...not-found]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='flex flex-col h-96 items-center justify-end font-bold text-4xl'>
+      <div>404</div>
+      <div>Not found</div>
+    </div>
+  );
+}
+
+export default NotFound;
