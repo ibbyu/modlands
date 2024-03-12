2c889e59ebce41d903f1ac51c34617e3f9f52fb2
Ibby <ibby364@outlook.com>
2024-03-12 16:30:40 +0000

    feat: mods tag filter

--- src/app/(main)/mods/page.tsx
+++ src/app/(main)/mods/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ModCard from './_components/mod-card';
 import SearchFilter from './_components/search-filter';
 import { getModsByQueryNameIncludeOwner } from '@/server/data/mod';
+import TagFilter from './_components/tag-filter';
 
 export function generateMetadata() {
   return {
@@ -19,14 +20,15 @@ const MODS_PER_PAGE = 10;
 const ModsPage = async ({ searchParams }: Props) => {
   const q = typeof searchParams.q === "string" ? searchParams.q : undefined;
   const page = typeof searchParams.p === "string" ? Number(searchParams.p) : 1;
+  const c = Array.isArray(searchParams.c) ? searchParams.c : [];
 
   const mods = await getModsByQueryNameIncludeOwner(q, MODS_PER_PAGE);
 
   return (
     <div className='pt-16'>
       <div className='grid grid-cols-1 md:grid-cols-8 gap-6 h-full'>
-        <div className='hidden md:flex flex-col rounded-2xl md:col-span-2 p-4 h-96 gap-4'>
-          placeholder
+        <div className='hidden md:flex flex-col rounded-2xl md:col-span-2 p-6 h-96 gap-4 bg-accent'>
+          <TagFilter activeTags={c}/>
         </div>
         <div className='w-full h-full md:col-span-6 rounded-2xl flex flex-col gap-4'>
           <SearchFilter q={q} page={page} totalMods={mods.length}/>
